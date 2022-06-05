12d1b8301b0cbbfe2ee927103190fe136b692d33
Benjamin Akar <benjaminakar2001@gmail.com>
2022-06-05 17:47:19 +0200

    refactor: fix build and remove some boilerplate code

--- apps/landing/renderer/_default.page.server.tsx
+++ apps/landing/renderer/_default.page.server.tsx
@@ -1,33 +1,32 @@
-import ReactDOMServer from 'react-dom/server'
-import React from 'react'
-import { PageShell } from './PageShell'
-import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
-import logoUrl from './logo.svg'
-import type { PageContext } from './types'
-import type { PageContextBuiltIn } from 'vite-plugin-ssr'
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr';
+import type { PageContextBuiltIn } from 'vite-plugin-ssr';
 
-export { render }
+import { PageShell } from './PageShell';
+import type { PageContext } from './types';
+
+export { render };
 // See https://vite-plugin-ssr.com/data-fetching
-export const passToClient = ['pageProps', 'urlPathname']
+export const passToClient = ['pageProps', 'urlPathname'];
 
 async function render(pageContext: PageContextBuiltIn & PageContext) {
-  const { Page, pageProps } = pageContext
-  const pageHtml = ReactDOMServer.renderToString(
-    <PageShell pageContext={pageContext}>
-      <Page {...pageProps} />
-    </PageShell>,
-  )
+	const { Page, pageProps } = pageContext;
+	const pageHtml = ReactDOMServer.renderToString(
+		<PageShell pageContext={pageContext}>
+			<Page {...pageProps} />
+		</PageShell>
+	);
 
-  // See https://vite-plugin-ssr.com/head
-  const { documentProps } = pageContext
-  const title = (documentProps && documentProps.title) || 'Vite SSR app'
-  const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
+	// See https://vite-plugin-ssr.com/head
+	const { documentProps } = pageContext;
+	const title = (documentProps && documentProps.title) || 'Vite SSR app';
+	const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr';
 
-  const documentHtml = escapeInject`<!DOCTYPE html>
+	const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
@@ -35,12 +34,12 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
       <body>
         <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
-    </html>`
+    </html>`;
 
-  return {
-    documentHtml,
-    pageContext: {
-      // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
-    },
-  }
+	return {
+		documentHtml,
+		pageContext: {
+			// We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
+		}
+	};
 }
